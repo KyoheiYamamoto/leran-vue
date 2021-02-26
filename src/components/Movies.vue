c1e3f4f99eb1f0781a9f493d45f652ef842908e4
KyoheiYamamoto <arienkyohei@gmail.com>
2021-02-26 09:21:33 +0900

    動画取得機能の実装完了

---
+++ src/components/Movies.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-row>
+    <v-col
+      v-for="item in movieInternalItems"
+      :key="item.id"
+      cols="12"
+      sm="6"
+      md="4"
+      style="text-align: center"
+    >
+      <div>
+        <iframe :src="item.url" width="290" height="163.25" frameborder="0" />
+        <p>{{ item.comment }}</p>
+      </div>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  props: {
+    movieItems: {
+      type: Array,
+      default: null,
+    },
+  },
+
+  data() {
+    return {
+      movieInternalItems: [],
+    };
+  },
+  methods: {
+    init() {
+      this.movieInternalItems = [];
+      Object.keys(this.movieItems).forEach((item) => {
+        console.log(this.movieItems[item]);
+        const newItem = {
+          id: this.movieItems[item].id,
+          url:"https://www.youtube.com/embed/" +
+              this.movieItems[item].url +
+              "?controls=1&loop=1&playlist=" +
+              this.movieItems[item].url,
+          comment: this.movieItems[item].comment,
+        };
+        this.movieInternalItems.push(newItem);
+      });
+    },
+  },
+};
+</script>
--- src/components/Top.vue
+++ src/components/Top.vue
@@ -5,27 +5,49 @@
       :formElevation="formElevation"
       @storeMovie="storeMovie"
     />
+    <movies
+      ref="movies"
+      :movie-items="movieItems"
+    />
   </v-container>
 </template>
 
 <script>
-  import InputForm from './InputForm.vue'
+  import InputForm from './InputForm'
+  import Movies from './Movies'
+  import axios from 'axios'
 
   export default {
     components: {
       InputForm,
+      Movies,
     },
 
     data () {
       return {
         formElevation: "10",
+        movieItems: [{}],
       }
     },
 
+    created() { //pageを実行した瞬間に行われる処理的な・・
+       this.getMovies()
+    },
+
     methods: {
+      getMovies () {
+        axios.get('https://youtube-curation.herokuapp.com/rest/1'
+        ).then((response) => {
+          this.movieItems = response.data.user.movies
+        }).catch((error) => {
+          console.log(error)
+        }).finally(() => {
+          this.$refs.movies.init()
+        })
+      },
       storeMovie (movieUrl, comment) {
         console.log(movieUrl, comment)
-      },
+      }
     },
   }
 </script>
